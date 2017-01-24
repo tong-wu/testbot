f536c9832f8eea01835567db09729558bd57c76c
Tong Wu <tong@anyperk.com>
2017-01-24 13:04:32 -0800

    test

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var controller = Botkit.slackbot({
   // reconnect to Slack RTM when connection goes bad
   retry: Infinity,
   debug: false,
-  interactive_replies: true
+  // interactive_replies: true
 });
 
 // Assume single team mode if we have a SLACK_TOKEN
